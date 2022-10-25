60c6450d227b90d6b98a2ca9934b0af6aa2512cf
Monica calle <monicacalle369@gmail.com>
2022-10-24 23:36:28 -0300

    fix: spelling error in main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.css";
 document.querySelector("#app").innerHTML = `
   <div  class="container">
     <input type="file" id="upload-button" multiple accept="image/*"/>
-    <label for="upload-button"><i class="fa-solid fa-upload"></i>&nbsp; Choose Or Drop Phothos </label>
+    <label for="upload-button"><i class="fa-solid fa-upload"></i>&nbsp; Choose Or Drop Photos </label>
     <div id="error">  </div>
     <div id="image-display"> 
   </div>
