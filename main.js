2629d319a87ac909be437b71c11326dbf7b42952
Monica calle <monicacalle369@gmail.com>
2022-10-24 23:30:26 -0300

    drag and drop app creation from scratch using vite + vanilla js

---
+++ main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div  class="container">
+    <input type="file" id="upload-button" multiple accept="image/*"/>
+    <label for="upload-button"><i class="fa-solid fa-upload"></i>&nbsp; Choose Or Drop Phothos </label>
+    <div id="error">  </div>
+    <div id="image-display"> 
+  </div>
+`;
+const script = document.createElement("script");
+script.src = "./script.js";
+document.body.appendChild(script);
